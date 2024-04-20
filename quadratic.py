07a303f6c6b3cba6a250df8fb5d69f89318325a6
FrancoAmbrosini <162723096+FrancoAmbrosini@users.noreply.github.com>
2024-04-20 18:38:17 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,60 @@
-# Replace the "ANSWER HERE" for your answer
+import math
 
-def roots(a, b, c):
-    return "ANSWER HERE"
+
+def roots(a,b,c):
+    D=b**2-4*a*c
+    x1= (-b+(D)**0.5)/2
+    x2= (-b-(D)**0.5)/2
+    if x1==x2:
+        return f'({x1})'
+    else:
+        if D>=0:
+            return f'({x1}, {x2})'
+        else:
+            return f'( )'
 
 
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
+        y= a*x**2+b*x+c
+        return y
 
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
+    y= f" {a} * X^2 + {b} * X + {c}"
+    y2= f" {a} * X^2"
+    y3= f" {a} * X^2 + {b} * X"
+    y4= f" {a} * X^2 + {c}"
+    y5= f" {b} * X"
+    y6= f" {b} * X + {c}"
+    y7= f" {c}"
+    if a!=0 and b!=0 and c!=0:
+        return f"f(x) ={y}"
+    elif a!=0 and b==0 and c==0:
+        return f"f(x) ={y2}"
+    elif a!=0 and b!=0 and c==0:
+        return f"f(x) ={y3}"
+    elif a!=0 and b==0 and c!=0:
+        return f"f(x) ={y4}"
+    elif a==0 and b!=0 and c==0:
+        return f"f(x) ={y5}"
+    elif a==0 and b!=0 and c!=0:
+        return f"f(x) ={y6}"
+    else:
+        return f"f(x) ={y7}"
 
 
-def derivation(a, b):
-    return "ANSWER HERE"
+
+def derivation(a, b, c):
+    d= f"{2*a} * X + {b}"
+    d2= f"{b}"
+    d3= f"{2*a} * X"
+    d4= 0
+    if a!=0 and b!=0:
+        return f"f'(x) = {d}"
+    elif a==0 and b!=0:
+        return f"f'(x) = {d2}"
+    elif a!=0 and b==0:
+        return f"f'(x) = {d3}"
+    else:
+        return f"f'(x) = {d4}"
+
